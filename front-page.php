38132a4c64ee8cd4ba80552fe91878b6037ea009
Dan <dan.stoakes8@gmail.com>
2023-01-28 19:52:42 +0000

    Add empty nav template file and adjust homepage

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div>
+<div id="home_page">
     <div>
         <p>Oops!</p>
         <p><?php esc_html_e("We can't seem to find the page you're looking for.", "catto-studios-wp-theme"); ?></p>
